2a6d534311ec95129bf50e25724b93cc6156b5b7
R-SAMUEL-EBENEZER <samebi.akm@gmail.com>
2024-01-26 09:31:14 +0530

    First commit

---
+++ day 3/tesk json.js
@@ -0,0 +1,20 @@
+//Object
+
+let obj1={ 
+    name:"preson 1",
+    age:5
+    };
+  let obj2={
+    age:5,
+    name:"preson 1"
+    };
+  
+  //json
+  let obj3={ 
+    "name":"preson 1",
+    "age":5
+    };
+  let obj4={"age":5,"name":"preson 1"
+  };
+  
+  console.log(obj1.name)
